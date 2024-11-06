a53931dba9f73dbd7bc3adda3fdfba8045d2eaa6
Eliezer Terrero Rojas <eliezer_terrero@ucne.edu.do>
2024-11-05 21:21:59 -0400

    Agrega las funciones GET por ID de los Clientes, Rentas y Vehiculos Ademas Agrega la funcion DeleteVehiculo en el RentCarRemoteDataSource

--- app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/RentCarRemoteDataSource.kt
+++ app/src/main/java/ucne/edu/proyectofinalaplicada2/data/remote/RentCarRemoteDataSource.kt
@@ -10,19 +10,24 @@ class RentCarRemoteDataSource @Inject constructor(
     private val rentCarApi: RentCarApi
 ) {
     //cliente
+    suspend fun getCliente(id: Int) = rentCarApi.getCliente(id)
     suspend fun getClientes() = rentCarApi.getClientes()
     suspend fun addCliente(clienteDto: ClienteDto) = rentCarApi.addCliente(clienteDto)
     suspend fun updateCliente(id: Int ,clienteDto: ClienteDto) = rentCarApi.updateCliente(id, clienteDto)
 
     //renta
+    suspend fun getRenta(id: Int) = rentCarApi.getRenta(id)
     suspend fun getRentas() = rentCarApi.getRentas()
     suspend fun addRenta(rentaDto: RentaDto) = rentCarApi.addRenta(rentaDto)
     suspend fun updateRenta(id: Int ,rentaDto: RentaDto) = rentCarApi.updateRenta(id, rentaDto)
 
     //vehiculo
+    suspend fun getVehiculo(id: Int) = rentCarApi.getVehiculo(id)
     suspend fun getVehiculos() = rentCarApi.getVehiculos()
     suspend fun addVehiculo(vehiculoDto: VehiculoDto) = rentCarApi.addVehiculo(vehiculoDto)
     suspend fun updateVehiculo(id: Int ,vehiculoDto: VehiculoDto) = rentCarApi.updateVehiculo(id, vehiculoDto)
+    suspend fun deleteVehiculo(id: Int) = rentCarApi.deleteVehiculo(id)
+
 
 
 }
